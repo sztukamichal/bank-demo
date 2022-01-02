439c7f4b25c2953abf056614fc5e3bf6566ed555
Michał Sztuka <michal.sztuka@deviniti.com>
2022-01-02 17:28:16 +0100

    Empty get mapping

--- src/main/java/com/example/bankdemo/AccountController.java
+++ src/main/java/com/example/bankdemo/AccountController.java
@@ -1,4 +1,15 @@
 package com.example.bankdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+
+    @GetMapping
+    public void getAccount() {
+    }
+
 }
--- src/test/java/com/example/bankdemo/BankDemoApplicationTests.java
+++ src/test/java/com/example/bankdemo/BankDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.bankdemo;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,10 +20,17 @@ class BankDemoApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("")
-    void mockMvcInitilized() {
-        assertThat(mockMvc).isNotNull();
+
+    @Nested
+    @DisplayName("Account controller should")
+    class AccountControllerTest {
+        @Test
+        @DisplayName("given get call should return ok status")
+        void shouldReturnOKResponseOnGetCall() throws Exception {
+            mockMvc.perform(MockMvcRequestBuilders.get("/account"))
+                    .andExpect(status().isOk());
+        }
+
     }
 
 }
